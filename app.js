eda243cefaa396b13f1281862ee227ef08cac1b3
cristina1084 <cristina.johns1084@gmail.com>
2019-08-06 11:55:17 +0530

    books display

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var exp = require('express');
 var bodyparser = require('body-parser');
 const app = exp();
-
+var book_arr = [{name:"Book1", auth:"Author1", price:890},{name:"Book2", auth:"Author2", price:90}, {name:"Book3", auth:"Author3", price:230}];
 app.set("view engine","ejs");  //setting view to ejs -- can be set to other templates also
 app.set("views","./src/views");  //specify folder where ejs pages are saved
 
@@ -21,6 +21,10 @@ app.post("/home",(req,res)=>{
         res.render("home",{user:req.body.uname}); //pass parameters as json
     else 
         res.redirect("/"); //redirect to login page
+})
+
+app.get("/books",(req,res)=>{
+    res.render("books",{books:book_arr});
 })
 
 app.listen(8080,()=>{
