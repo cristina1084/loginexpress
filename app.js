9c2806cc2bc555885084992869a90ab1a5be2e65
cristina1084 <cristina.johns1084@gmail.com>
2019-08-06 10:25:51 +0530

    login using express

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ var exp = require('express');
 var bodyparser = require('body-parser');
 const app = exp();
 
+app.use(bodyparser.urlencoded({extended:true}));
+
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/src/views/login.html")
 })
 
-app.get("/home",(req,res)=>{
-    res.sendFile(__dirname+"/src/views/home.html")
-    console.log(req.body.name);
+app.post("/home",(req,res)=>{
+    console.log(req.body.uname, req.body.pwd);
+    if(req.body.uname=="admin" && req.body.pwd=="Pass@123")
+        res.sendFile(__dirname+"/src/views/home.html")
+    else 
+        res.sendFile(__dirname+"/src/views/login.html")
     
 })
 
